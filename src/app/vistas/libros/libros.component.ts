0ee145bc295315316573146889cf7e51b49f2b9f
stivenz12 <estivendzp239@gmail.com>
2024-09-10 17:03:17 -0500

    mi primer commit

---
+++ src/app/vistas/libros/libros.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-libros',
+  standalone: true,
+  imports: [],
+  templateUrl: './libros.component.html',
+  styleUrl: './libros.component.scss'
+})
+export class LibrosComponent {
+
+}
